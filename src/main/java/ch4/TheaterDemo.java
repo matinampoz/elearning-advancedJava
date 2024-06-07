ab66b4f2d6fb1a687e03a7b45163eb8476fc5cc5
matinampoz <matinampoz@gmail.com>
2024-06-07 11:01:40 +0300

    adds

---
+++ src/main/java/ch4/TheaterDemo.java
@@ -0,0 +1,67 @@
+package ch4;
+
+import java.util.Scanner;
+
+public class TheaterDemo {
+
+    private static Theater theater = new Theater("Acropolis", 10, 12);
+    private static Scanner in = new Scanner(System.in); //εφοσον η main ειναι static μπορει να καλει μόνο static μεθοδους και μεταβλητες
+
+    public static void main(String[] args) {
+
+        boolean exit = false;
+        int choice;
+
+        do {
+            showMenu();
+            String response = in.nextLine();
+            if (response.matches("\\d")) {
+                choice = Integer.parseInt(response);
+            } else {
+                System.out.println("Wrong choice");
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    //manageReservation();
+                    break;
+                case 2:
+                    //manageCancelation();
+                    break;
+                case 3:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Please give choice 1, 2 or 3");
+            }
+        } while (!exit);
+        System.out.println("Adios");
+    }
+
+    public static void showMenu() {
+        System.out.println("Give your choice");
+        System.out.println("1. Reserve seat");
+        System.out.println("2. Cancel seat");
+        System.out.println("3. Exit");
+    }
+
+    public static void manageReservation(){
+        try{
+            System.out.println("Give seat number");
+            String response = in.nextLine();
+        /*
+        ο ελεγχος αν ειναι σωστο το response θα γινει στο reserveSeat().
+        εδω φτιαχνουμε εναν ενδιαμεσο (controller). Απο την μια μερια θα επικοινωνει με τον χρήστη και από την αλλη θα
+        καλει τις αντιστοιχες μεθοδους. Δεν εχει λογική ο controller απλα αλληλεπιδρα με τον χρηστη, λαμβανει δεδομενα
+        και καλει μεθοδους. Τo Theater που ειναι ενα business object εχει την λογική
+         */
+            System.out.printf("%Successful registration\n", theater.reserveSeat(response) ? " " : "Unsuccessful registration");
+        } catch (IsReservedException e) {
+            System.out.println("Seat is already reserved");
+        } finally {
+            theater.printSeats();
+        }
+
+    }
+}
